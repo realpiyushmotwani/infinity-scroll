85dcc9b38b6eb059d92b15345a1dc0614502af60
Piyush Motwani <1032210487@mitwpu.edu.in>
2023-07-06 00:18:19 +0530

    final changes

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
-const apiKey = "RKKvmmzQwhD4Pus_E118pNp9eLr2HgD37VgDsKPe4C0";
+const count = 20;
+const apiKey = "k4t85ARjMMe_kKoUpkX6Od2GojBpzC8mECP8bqX3aYc";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-function setAttributes(element, attribute)
-{
-    for(const key in attribute)
-    {
-        element.setAttribute(key,attribute[key])
-    }
+function imageLoaded() {
+  imagesLoaded++;
+
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+function setAttributes(element, attribute) {
+  for (const key in attribute) {
+    element.setAttribute(key, attribute[key]);
+  }
 }
 // create element s for links and photos , add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     //create link tag to link to unsplash
-    const item = document.createElement('a');
+    const item = document.createElement("a");
     // item.setAttribute("href", photo.links.html);
     // item.setAttribute("target", "_blank");
-    setAttributes(item,{
-        href:photo.links.html,
-        target: "_blank"
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
 
-
-    })
-
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     // img.setAttribute("src", photo.urls.regular);
     // img.setAttribute("alt", photo.alt_description);
     // img.setAttribute("title", photo.alt_description);
-    setAttributes(img,{
-        src:photo.urls.regular,
-        alt: photo.alt_description,
-        title: photo.alt_description
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
 
-    })
+    img.addEventListener("load", imageLoaded);
 
     item.appendChild(img);
     imageContainer.append(item);
@@ -51,8 +63,21 @@ async function getPhotos() {
     photosArray = await resp.json();
     displayPhotos();
     console.log(photosArray);
-  } catch (err) {}
+  } catch (err) {
+    console.log("ERROR");
+  }
 }
+
+// check to see if scrolling near bottom of page, then load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // on load
 getPhotos();
